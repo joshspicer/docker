b7c7c8b6a15ffba9b5aa029a2237006a400ed1a0
Josh Spicer <hello@joshspicer.com>
2021-01-11 17:06:52 -0500

    Update upload-vsfeedback.sh

--- scripts/upload-vsfeedback.sh
+++ scripts/upload-vsfeedback.sh
@@ -2,10 +2,13 @@
 
 if [ -z "$KEY" ]; then 
     KEY="shouldprobsSETSOMETHING3lse"
+else
+    read -sp 'Password: ' $KEY
 fi
 
-GPG_TTY=$(tty) \
-&& export GPG_TTY \
-&& cd /tmp \
-&& gpg --symmetric --batch --yes --passphrase $KEY VSFeedbackVSRTCLogs.zip \
-&& curl --upload-file VSFeedbackVSRTCLogs.zip.gpg https://free.keep.sh
+# GPG_TTY=$(tty) \
+# && export GPG_TTY \
+
+zip /tmp/VSFeedbackVSRTCLogs.zip /tmp/VSFeedbackVSRTCLogs \
+&& gpg --symmetric --batch --yes --passphrase $KEY /tmp/VSFeedbackVSRTCLogs.zip \
+&& curl --upload-file /tmp/VSFeedbackVSRTCLogs.zip.gpg https://free.keep.sh
